e16925e08d841aa3e88c2aba81369d0839923b6d
Harsher Gill <gillharsher1@gmail.com>
2023-08-16 09:25:23 +0530

    tried using retrofit for api calls

---
+++ lib/features/daily_news/data/data_sources/remote/news_api_service.dart
@@ -0,0 +1,21 @@
+// import 'dart:io';
+
+import 'package:learning/core/constants/constants.dart';
+import 'package:learning/features/daily_news/data/models/article.dart';
+import 'package:retrofit/retrofit.dart';
+// ignore: depend_on_referenced_packages
+import 'package:dio/dio.dart';
+
+part 'news_api_service.g.dart';
+ 
+@RestApi(baseUrl: newsAPIBaseURL)
+abstract class NewsApiService {
+  factory NewsApiService(Dio dio) = _NewsApiService;
+
+  @GET('/top-headlines')
+  Future<HttpResponse<List<ArticleModel>>> getNewsArticles({
+    @Query("apiKey") String ? apiKey,
+    @Query("country") String ? country,
+    @Query("category") String ? category,
+  });
+}
